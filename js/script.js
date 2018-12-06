e058f592baa09b86a769b3f124166f5adeaa9aef
Hazim Sami <hazimisnot@gmail.com>
2018-12-06 19:27:12 +0200

    code updates

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
-var car  = {
-			make: 'VW',
-			type: 'Polo',
-			color: 'blue',
-			isTurnedOn: false,
-			numberOfWheels: 4,
-			seats: ['seat 1', 'seat 2', 'seat 3', 'seat 4'],
-			turnOn: function () {
+/**
+var favColor = 'red';
+var myArray = ['blue', 'red', 'green'];
+var numofFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstname: 'Rich',
+	lastName: 'Armstrong',
+	yearsAlive: 103,
+	isMale: true
+};
+**/
+
+var car = {
+	make: 'VW',
+	type: 'Polo',
+	color: 'blue',
+	isTurnedOn: false,
+	numberOfWheels: 4,
+	seats: [
+		'seat 1', 
+		'seat 2', 
+		'seat 3', 
+		'seat 4'
+		],
+		turnOn: function () {
+			this.isTurnedOn = true;
+		},
+		fly: function () {
+			alert('fly');
+		},
+		switchCar: function (isOn) {
+			console.log('turn car ' + isOn);
+			if (isOn == true) {
 				this.isTurnedOn = true;
+			} else {
+				this.isTurnedOn = false;
 			}
-		};
+		}
+};
